1aa37703b6368327d41fcab1d2332d73197397b3
ProgFabs <earthbornshepard@hotmail.com>
2020-05-15 16:20:59 -0300

    exercícios concluídos, desafios parcialmente concluídos

---
+++ whats4/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FormMensagem from './components/Inputs/FormMensagem.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FormMensagem />
+    </div>
+  );
+}
+
+export default App;
